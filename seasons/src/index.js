44eaac060b31c16b3d09f52015d8e0096763ca3d
Ryan Gleason <ryangleason82@gmail.com>
2019-05-06 05:49:32 -0400

    Added state manipulation

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,16 +6,20 @@ class App extends React.Component {
 		// React.Component has a constructor. This overrides that function. Super makes sure that the parents constructor function is still called so we can use its functionality.
 		super(props);
 
+		// Only time we do direct assignment.
 		this.state = { lat: null };
-	}
 
-	//React says we must define render!
-	render() {
 		window.navigator.geolocation.getCurrentPosition(
-			position => console.log(position),
+			position => {
+				// need to call setState. No direct assignments!
+				this.setState({ lat: position.coords.latitude });
+			},
 			err => console.log(err)
 		);
-		return <div>Latitude: </div>;
+	}
+	//React says we must define render!
+	render() {
+		return <div>Latitude: {this.state.lat}</div>;
 	}
 }
 
