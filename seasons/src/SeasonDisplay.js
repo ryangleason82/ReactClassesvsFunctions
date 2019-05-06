59c23794ab463d277d2c584a33dc5d24d95e6907
Ryan Gleason <ryangleason82@gmail.com>
2019-05-06 07:02:58 -0400

    slight refactor

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -10,9 +10,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
 	const season = getSeason(props.lat, new Date().getMonth());
-
-	console.log(season);
-	return <div>Season Display</div>;
+	const text =
+		season === "Winter" ? "Burr, it is chilly!" : "Lets hit the beach!";
+	return (
+		<div>
+			<h1>{text}</h1>
+		</div>
+	);
 };
 
 export default SeasonDisplay;
