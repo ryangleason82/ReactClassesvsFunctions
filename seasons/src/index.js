4a5856d2f6926e2353f6689e608013629ea7c283
Ryan Gleason <ryangleason82@gmail.com>
2019-05-06 06:58:48 -0400

    Update seasonDisplay component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,21 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-	constructor(props) {
-		// React.Component has a constructor. This overrides that function. Super makes sure that the parents constructor function is still called so we can use its functionality.
-		super(props);
-
-		// Only time we do direct assignment.
-		this.state = { lat: null, errorMessage: "" };
-	}
+	state = { lat: null, errorMessage: "" };
 
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			position => {
-				// need to call setState. No direct assignments!
-				this.setState({ lat: position.coords.latitude });
-			},
+			position => this.setState({ lat: position.coords.latitude }),
 			err => this.setState({ errorMessage: err.message })
 		);
 	}
@@ -30,7 +22,7 @@ class App extends React.Component {
 			return <div>Error: {this.state.errorMessage}</div>;
 		}
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
 		return <div>Loading!</div>;
