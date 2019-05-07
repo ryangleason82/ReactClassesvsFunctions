301b4d40fbc73fac6780e83604bac71ba98a381e
Ryan Gleason <ryangleason82@gmail.com>
2019-05-07 06:27:19 -0400

    Added spinner, finished up

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,10 @@
 import React from "react";
+import "./SeasonDisplay.css";
+
+const seasonConfig = {
+	Summer: { text: "Lets hit the beach!", iconName: "sun" },
+	Winter: { text: "Burr it is cold!", iconName: "snowflake" }
+};
 
 const getSeason = (lat, month) => {
 	if (month > 2 && month < 9) {
@@ -10,11 +16,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
 	const season = getSeason(props.lat, new Date().getMonth());
-	const text =
-		season === "Winter" ? "Burr, it is chilly!" : "Lets hit the beach!";
+	const { text, iconName } = seasonConfig[season];
 	return (
-		<div>
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left massive ${iconName} icon`} />
 			<h1>{text}</h1>
+			<i className={`icon-right massive ${iconName} icon`} />
 		</div>
 	);
 };
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
 	state = { lat: null, errorMessage: "" };
@@ -16,8 +17,7 @@ class App extends React.Component {
 		console.log("Update!");
 	}
 
-	//React says we must define render!
-	render() {
+	renderContent() {
 		if (this.state.errorMessage && !this.state.lat) {
 			return <div>Error: {this.state.errorMessage}</div>;
 		}
@@ -25,7 +25,11 @@ class App extends React.Component {
 			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
-		return <div>Loading!</div>;
+		return <Spinner message="Accept location request yo" />;
+	}
+
+	render() {
+		return <div className="border red">{this.renderContent()}</div>;
 	}
 }
 
